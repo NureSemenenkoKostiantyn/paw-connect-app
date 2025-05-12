e2d37566bc4183e6a44e7a7805741db52f26acf6
Kostiantyn Semenenko <major7577@gmail.com>
2025-05-12 01:28:05 +0100

    User controller implemented

--- backend/src/main/java/com/pawconnect/backend/user/controller/UserController.java
+++ backend/src/main/java/com/pawconnect/backend/user/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.pawconnect.backend.user.controller;
 
-import com.pawconnect.backend.user.model.User;
+import com.pawconnect.backend.user.dto.CurrentUserResponse;
+import com.pawconnect.backend.user.dto.PublicUserResponse;
+import com.pawconnect.backend.user.dto.UserUpdateProfileRequest;
 import com.pawconnect.backend.user.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -13,6 +15,27 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/current")
+    public ResponseEntity<CurrentUserResponse> getCurrentUser() {
+        return ResponseEntity.ok(userService.getCurrentUser());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PublicUserResponse> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getPublicUserById(id));
+    }
+
+    @PutMapping("/current")
+    public ResponseEntity<CurrentUserResponse> updateCurrentUser(@RequestBody UserUpdateProfileRequest currentUser) {
+        return ResponseEntity.ok(userService.updateCurrentUserProfile(currentUser));
+    }
+
+    @DeleteMapping("/current")
+    public ResponseEntity<Void> deleteCurrentUser() {
+        userService.deleteCurrentUser();
+        return ResponseEntity.noContent().build();
     }
 
 }
