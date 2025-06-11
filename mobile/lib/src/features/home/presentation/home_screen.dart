564dc2d5d1105e89448bef54eb6d3c5c826d50de
Kostiantyn Semenenko <major7577@gmail.com>
2025-06-11 19:45:01 +0100

    home

--- mobile/lib/src/features/home/presentation/home_screen.dart
+++ mobile/lib/src/features/home/presentation/home_screen.dart
@@ -69,7 +69,7 @@ class _HomeScreenState extends State<HomeScreen> {
             }
             return true;
           },
-          cardBuilder: (context, index) {
+          cardBuilder: (context, index, horizontalOffset, verticalOffset) {
             return CandidateCard(candidate: _candidates[index]);
           },
         ),
