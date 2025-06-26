ca4fc666d151c5e70ff9589c84cfb91c0a8c0386
Kostiantyn Semenenko <major7577@gmail.com>
2025-06-26 03:52:10 +0100

    Allow principal auth in chat messages

--- backend/src/main/java/com/pawconnect/backend/chat/controller/ChatController.java
+++ backend/src/main/java/com/pawconnect/backend/chat/controller/ChatController.java
@@ -6,7 +6,11 @@ import com.pawconnect.backend.chat.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,7 +20,10 @@ public class ChatController {
     private final SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping("/chat.send")
-    public void sendMessage(ChatMessageRequest request) {
+    public void sendMessage(Principal principal, ChatMessageRequest request) {
+        if (principal instanceof Authentication authentication) {
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
         ChatMessageResponse msg = messageService.saveMessage(request);
         messagingTemplate.convertAndSend("/topic/chats/" + msg.getChatId(), msg);
     }
